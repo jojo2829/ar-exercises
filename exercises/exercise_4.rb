b87b16489c5bd9e41bf749e093c5d73d591bcfe8
jojo2829 <joulinli@gmail.com>
2021-07-20 05:53:46 +0000

    display mens store and womens store with annual revenue less than 1m

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store = Store.new(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store.save
+store = Store.new(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store.save
+store = Store.new(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+store.save
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "store name: #{store.name}, annual revenue: #{store.annual_revenue}"
+end
+
+@mwomens_stores = Store.where(womens_apparel: true)
+
+@mwomens_stores.each do |store|
+  if (store.annual_revenue < 1000000)
+    puts "store name: #{store.name}, annual revenue: #{store.annual_revenue}"
+  end
+end
